6f74424fcdef655a7b8e3accbdef19a19b167dc2
bede abbe <bede.abbe91@gmail.com>
2022-01-14 17:30:22 +0000

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Atengdem</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Agroconsultancy">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{csrf_token()}}">
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.12.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+<link rel="stylesheet" type="text/css" href="{{ asset('bootstrap-4.3.1-dist/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/custom.css') }}">
+<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+ 
+  <!-- <link rel="shortcut icon" href="{{ asset('assets/images/logo4.png') }}" type="image/x-icon"> -->
+
+</head>
+
+<body>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarcollapse">
+            <ul class="navbar-nav justify-content-between w-100">
+                <li><a href="#" class="nav-link active">Home</a></li>
+                <li><a href="#" class="nav-link dubaiTrip">Dubai Trip</a></li>
+                <li><a href="#" class="nav-link chinaTrip">China Trip</a></li>
+                <li><a href="#" class="nav-link wedding">Wedding</a></li>
+                <li><a href="#" class="nav-link other">Other</a></li>
+            </ul>
+        </div>
+    </nav>
+
+</div>
+  <div class="container" style="padding-top: 10%; text-align: center;">
+           <div class="row" style="padding-bottom: 20px;">
+               <div class="col-md-4 col-sm-12 mt-2">
+                 <div class="rectangle-large-vertical full-image box-shadow-1 image_" data-aos="fade-right" style="background-image: url('{{  asset('images/IMG_8922.JPG') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_8922.JPG') }}')">
+                   
+                 </div>
+               </div>
+               <div class="col-4 mt-2">
+
+               </div>
+               <div class="col-md-4 col-sm-12 mt-2">
+                 <div class="rectangle-large-vertical float-right full-image box-shadow-1 image_" data-aos="fade-left" style="background-image: url('{{  asset('images/IMG_9488.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_9488.jpg') }}')">
+                   
+                 </div>
+               </div>
+             </div>
+  </div>
+  <div class="container-fluid">
+              <div class="row medium-padding grey-background">
+               <div class="col-md-3 col-sm-12 mt-2">
+                 
+               </div>
+
+               <div class="col-md-3 col-sm-6 mt-2">
+                 <div class="rectangle-large-horizontal-full-width box-shadow-4 full-image image_" data-aos="fade-left" data-aos-delay="20" data-aos-duration="800" data-aos-offset="200" style="background-image: url('{{  asset('images/IMG_7142.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_7142.jpg') }}')">
+                  
+                 </div>
+               </div>
+
+               <div class="col-md-3 col-sm-6 mt-2">
+                 <div class="rectangle-large-horizontal-full-width box-shadow-4 full-image image_" data-aos="fade-left" data-aos-delay="100" data-aos-duration="1500" data-aos-offset="300" style="background-image: url('{{  asset('images/PHOTO-2019-09-20-18-58-52.JPG') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/PHOTO-2019-09-20-18-58-52.JPG') }}')">
+                   
+                 </div>
+               </div>
+               <div class="col-md-3 col-sm-12 mt-2">
+                 <div class="rectangle-large-horizontal-full-width box-shadow-4 full-image image_" data-aos="fade-left" data-aos-delay="180" data-aos-duration="2000" data-aos-offset="400" style="background-image: url('{{  asset('images/IMG_3262.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_3262.jpg') }}')">
+                  
+                 </div>
+               </div>
+           </div>
+    </div>
+    <div class="container">
+      <div class="row small-padding">
+             <div class="col-8">
+               <div class="large-circle full-image box-shadow-5 image_" data-aos="fade-right" data-aos-delay="10" style="background-image: url('{{  asset('images/PHOTO-2019-09-20-18-53-07.JPG') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/PHOTO-2019-09-20-18-53-07.JPG') }}')">
+                 
+               </div>
+             </div>
+              <div class="col-4">
+                <div class="smaller-circle full-image box-shadow-5 image_" data-aos="fade-left" data-aos-delay="50" style="background-image: url('{{  asset('images/IMG_4005_2.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_4005_2.jpg') }}')">
+                  
+                </div>
+                <div class="small-circle full-image box-shadow-5 image_" style="position: absolute; right: 150px; top: 80px; background-image: url('{{  asset('images/IMG_9530.JPG') }}')" data-aos="fade-left" data-aos-delay="100" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_9530.JPG') }}')">
+                  
+                </div>
+                <div class="medium-circle full-image box-shadow-5 image_" style="position: absolute; left: 0; bottom: 0; background-image: url('{{  asset('images/IMG_9569.jpg') }}')" data-aos="fade-left" data-aos-delay="150" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_9569.jpg') }}')">
+                  
+                </div>
+              </div>
+      </div>
+      <hr>
+    </div>
+    
+    <div class="container mt-4 small-padding dubaiTripDiv">
+      <h1 class="text-muted"> Dubai Trip</h1>
+      <div class="row small-padding">
+        <div class="col-6">
+          <div class="row">
+            <div class="col-12">
+              <div class="medium-square full-image box-shadow-4 image_" data-aos="zoom-out-left" data-aos-delay="10" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/IMG_2106.jpg') }}')" onclick="showImg('{{  asset('images/IMG_2106.jpg') }}')">
+              </div>
+            </div>
+            <div class="col-12 mt-4">
+              <div class="large-square full-image box-shadow-4 image_" data-aos="zoom-out-left" data-aos-delay="80" data-aos-duration="1000" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/IMG_3831.JPG') }}')" onclick="showImg('{{  asset('images/IMG_3831.JPG') }}')">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 small-padding">
+          <div class="large-square full-image box-shadow-4 image_" data-aos="zoom-out-right" data-aos-delay="50" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/IMG_1537.jpg') }}')" onclick="showImg('{{  asset('images/IMG_1537.jpg') }}')">
+            
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="row small-padding">
+            <div class="mx-auto col-8">
+              <div class=" mx-auto large-square full-image box-shadow-4 image_" data-aos="zoom-out-left" data-aos-delay="30" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/IMG_9530.JPG') }}')" onclick="showImg('{{  asset('images/IMG_9530.JPG') }}')">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr>
+    </div>
+
+    <div class="container">
+      <h1 class="text-muted"> Wedding </h1>
+      <div class="row small-padding weddingDiv">
+        <div class="col-6 mt-2">
+          <div class="row">
+              <div class="col-12">
+                <div class="large-square full-image box-shadow-3 float-right image_" data-aos="zoom-out-left" data-aos-delay="10" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/PHOTO-2019-09-20-18-53-07 2.JPG') }}')" onclick="showImg('{{  asset('images/PHOTO-2019-09-20-18-53-07 2.JPG') }}')">
+                </div>
+              </div>
+              <div class="col-6">
+              </div>
+              <div class="col-6 mt-4">
+                <div class="medium-square full-image box-shadow-3 float-left image_" data-aos="zoom-out-left" data-aos-delay="10" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/PHOTO-2019-09-22-22-29-12 2.JPG') }}')" onclick="showImg('{{  asset('images/PHOTO-2019-09-22-22-29-12 2.JPG') }}')">
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="col-6 small-padding mt-2">
+          <div class="row">
+            <div class="col-6 mt-2">
+              <div class="large-square full-image box-shadow-3 image_" data-aos="zoom-out-left" data-aos-delay="200" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/PHOTO-2019-09-20-18-58-48.JPG') }}')" onclick="showImg('{{  asset('images/PHOTO-2019-09-20-18-58-48.JPG') }}')">
+              </div>
+            </div> 
+            <div class="col-6 mt-2">
+              
+            </div> 
+            <div class="col-6 mt-2">
+              
+            </div> 
+            <div class="col-6 mt-2">
+              <div class="medium-square full-image box-shadow-3 image_" data-aos="zoom-out-left" data-aos-delay="90" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/PHOTO-2019-09-22-22-28-12.JPG') }}')" onclick="showImg('{{  asset('images/PHOTO-2019-09-22-22-28-12.JPG') }}')">
+              </div>
+            </div> 
+          </div>
+        </div>
+        <div class="col-12 mt-1">
+          <div class="row">
+            <div class="mx-auto col-8">
+              <div class=" mx-auto large-square full-image box-shadow-3 image_" data-aos="fade-left" data-aos-delay="90" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/PHOTO-2019-09-20-18-58-52.JPG') }}')" onclick="showImg('{{  asset('images/PHOTO-2019-09-20-18-58-52.JPG') }}')">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-2 mt-4">
+        </div>
+        <div class="col-4 mt-4">
+          <div class=" mx-auto medium-square full-image box-shadow-3 image_" data-aos="zoom-out-left" data-aos-delay="40" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/PHOTO-2019-09-22-22-28-17.JPG') }}')" onclick="showImg('{{  asset('images/PHOTO-2019-09-22-22-28-17.JPG') }}')">
+          </div>
+        </div>
+        <div class="col-6 mt-4">
+        </div>
+        <div class="col-7 mt-4">
+        </div>
+        <div class="col-4">
+          <div class=" mx-auto rectangle-large-horizontal-full-width full-image box-shadow-3 image_" data-aos="zoom-out-right" data-aos-delay="120" data-toggle="modal" data-target="#viewImage" style="background-image: url('{{  asset('images/PHOTO-2019-09-22-22-29-13.JPG') }}')" onclick="showImg('{{  asset('images/PHOTO-2019-09-22-22-29-13.JPG') }}')">
+          </div>
+        </div>
+        <div class="col-1">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container-fluid grey-background">
+      <div class="row small-padding otherDiv">
+          <div class="col-12">
+            <h1 class="text-muted"> See all</h1>
+          </div>
+          <div class="col-12">
+            <div class="row gx-5">
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-right" data-aos-delay="10" style="background-image: url('{{  asset('images/IMG_1930.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_1930.jpg') }}')">
+            
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-left" data-aos-delay="20" style="background-image: url('{{  asset('images/IMG_1958_2.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_1958_2.jpg') }}')">
+            
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-right" data-aos-delay="30" style="background-image: url('{{  asset('images/IMG_2899.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_2899.jpg') }}')">
+            
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-left" data-aos-delay="40" style="background-image: url('{{  asset('images/IMG_7142.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_7142.jpg') }}')">
+            
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-right" data-aos-delay="50" style="background-image: url('{{  asset('images/IMG_9569.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_9569.jpg') }}')">
+            
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-left" data-aos-delay="60" style="background-image: url('{{  asset('images/IMG_7410.jpg') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/IMG_7410.jpg') }}')">
+            
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-right" data-aos-delay="70" style="background-image: url('{{  asset('images/PHOTO-2019-09-20-18-58-48.JPG') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/PHOTO-2019-09-20-18-58-48.JPG') }}')">
+            
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mt-2">
+              <div class="fill-square mx-auto full-image box-shadow-3 image_" data-aos="flip-left" data-aos-delay="70" style="background-image: url('{{  asset('images/PHOTO-2019-09-22-22-28-12.JPG') }}')" data-toggle="modal" data-target="#viewImage" onclick="showImg('{{  asset('images/PHOTO-2019-09-22-22-28-12.JPG') }}')">
+            
+              </div>
+            </div>
+            </div>
+          </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="container">
+        <div class="row mt-4">
+          <div class="mx-auto">
+            <h5>Copyright</h5>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+
+    <!-- Modal -->
+<div class="modal fade" id="viewImage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+<!--       <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div> -->
+      <div class="modal-body">
+        <div class="image_modal" id="image_modal">
+          
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js') }}"></script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script>
+          AOS.init();
+
+          $(".dubaiTrip").click(function() {
+            $([document.documentElement, document.body]).animate({
+              scrollTop: $(".dubaiTripDiv").offset().top
+              }, 1000);
+            });
+
+          $(".chinaTrip").click(function() {
+            $([document.documentElement, document.body]).animate({
+              scrollTop: $(".chinaTripDiv").offset().top
+              }, 1000);
+            });
+
+          $(".wedding").click(function() {
+            $([document.documentElement, document.body]).animate({
+              scrollTop: $(".weddingDiv").offset().top
+              }, 1000);
+            });
+
+          $(".other").click(function() {
+            $([document.documentElement, document.body]).animate({
+              scrollTop: $(".otherDiv").offset().top
+              }, 1000);
+            });
+
+
+          function showImg(imageUrl) {
+            document.getElementById("image_modal").innerHTML = "<img class='img-fluid' src='"+imageUrl+"'>";
+          }
+        </script>
+</body>
